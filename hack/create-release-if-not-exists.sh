aa59152fc9e101842dfe27df7d40a1bacfb5ac9f
Hidetake Iwata <int128@gmail.com>
2023-07-31 12:51:07 +0900

    Post comment on pull request

--- hack/create-release-if-not-exists.sh
+++ hack/create-release-if-not-exists.sh
@@ -1,9 +1,25 @@
 #!/bin/bash
 set -eux
+
+# find the runner version in Dockerfile
 actions_runner_version="$(grep RUNNER_VERSION= Dockerfile | cut -f2 -d=)"
 
 if gh release view "$actions_runner_version"; then
-  exit # already exists
+  if [[ $GITHUB_EVENT_NAME == pull_request ]]; then
+    gh pr comment --body-file - <<EOF
+:warning: After merge, you may need to create a next release.
+EOF
+  fi
+
+  # the corresponding release already exists
+  exit
+fi
+
+if [[ $GITHUB_EVENT_NAME == pull_request ]]; then
+  gh pr comment --body-file - <<EOF
+:warning: After merge, GitHub Actions will automatically create a new release ${actions_runner_version}.
+EOF
+  exit
 fi
 
 # create a release if not exists
