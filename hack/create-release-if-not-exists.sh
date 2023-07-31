29f1eb00d8cc96651ca0e88ccdb63f659d559370
Hidetake Iwata <int128@gmail.com>
2023-07-31 13:28:56 +0900

    Fix

--- hack/create-release-if-not-exists.sh
+++ hack/create-release-if-not-exists.sh
@@ -7,11 +7,15 @@ actions_runner_version="$(grep RUNNER_VERSION= Dockerfile | cut -f2 -d=)"
 # post a comment if pull request
 if [[ $GITHUB_EVENT_NAME == pull_request ]]; then
   if gh release view "$actions_runner_version"; then
-    gh pr comment "$GITHUB_HEAD_REF" --body ":warning: After merge, you may need to create a next release."
+    gh pr comment "$GITHUB_HEAD_REF" --body-file - <<EOF
+:warning: If you need to release this change, [create a next release](https://github.com/quipper/actions-runner/releases/new) after merge.
+EOF
     exit
   fi
 
-  gh pr comment "$GITHUB_HEAD_REF" --body ":warning: After merge, GitHub Actions will automatically create a release ${actions_runner_version}."
+  gh pr comment "$GITHUB_HEAD_REF" --body-file - <<EOF
+:robot: GitHub Actions will automatically create a release ${actions_runner_version} after merge.
+EOF
   exit
 fi
 
