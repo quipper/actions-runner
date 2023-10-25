d9380276ac55666a43696a29d488430e91f0d3a1
Hidetake Iwata <int128@gmail.com>
2023-10-25 16:09:30 +0900

    Set --strip-components=1

--- hostedtoolcache/actions-setup-node.sh
+++ hostedtoolcache/actions-setup-node.sh
@@ -17,14 +17,17 @@ test "${NODE_VERSION}"
 
 function download_nodejs () {
   mkdir -p "${RUNNER_TOOL_CACHE}/node/${NODE_VERSION}/${NODE_ARCH}"
+  # For example, download https://nodejs.org/dist/v18.18.2/node-v18.18.2-linux-x64.tar.gz and
+  # extract to /opt/hostedtoolcache/node/18.18.2/x64
   curl -sf -L "https://nodejs.org/dist/v${NODE_VERSION}/node-v${NODE_VERSION}-${TARGETOS}-${NODE_ARCH}.tar.gz" |
-    tar -xzf - -C "${RUNNER_TOOL_CACHE}/node/${NODE_VERSION}/${NODE_ARCH}"
+    tar -xzf - --strip-components=1 -C "${RUNNER_TOOL_CACHE}/node/${NODE_VERSION}/${NODE_ARCH}"
   touch "${RUNNER_TOOL_CACHE}/node/${NODE_VERSION}/${NODE_ARCH}.complete"
   chown -R runner:docker "${RUNNER_TOOL_CACHE}/node"
 }
 
 function verify_nodejs () {
-  "${RUNNER_TOOL_CACHE}/node/${NODE_VERSION}/${NODE_ARCH}/node-v${NODE_VERSION}-${TARGETOS}-${NODE_ARCH}/bin/node" --version
+  # e.g., /opt/hostedtoolcache/node/18.18.2/x64/bin/node
+  "${RUNNER_TOOL_CACHE}/node/${NODE_VERSION}/${NODE_ARCH}/bin/node" --version
 }
 
 download_nodejs
