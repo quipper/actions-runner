c88721a4ba78a21643071bf0e25afa74612d8871
Hidetake Iwata <int128@gmail.com>
2023-10-25 15:39:40 +0900

    Refactor

--- hostedtoolcache/actions-setup-node.sh
+++ hostedtoolcache/actions-setup-node.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # This script downloads the specific version of Node.js into RUNNER_TOOL_CACHE directory.
+# It is designed to work with https://github.com/actions/setup-node
 set -eux -o pipefail
 
 : "${TARGETARCH}"
@@ -17,9 +18,9 @@ test "${NODE_VERSION}"
 function download_nodejs () {
   mkdir -p "${RUNNER_TOOL_CACHE}/node/${NODE_VERSION}/${NODE_ARCH}"
 
-  curl -sf -o /tmp/node.tar.gz -L "https://nodejs.org/dist/v${NODE_VERSION}/node-v${NODE_VERSION}-${TARGETOS}-${NODE_ARCH}.tar.gz"
-  tar -xzf /tmp/node.tar.gz -C "${RUNNER_TOOL_CACHE}/node/${NODE_VERSION}/${NODE_ARCH}"
-  rm -v /tmp/node.tar.gz
+  curl -sf -o node.tar.gz -L "https://nodejs.org/dist/v${NODE_VERSION}/node-v${NODE_VERSION}-${TARGETOS}-${NODE_ARCH}.tar.gz"
+  tar -xzf node.tar.gz -C "${RUNNER_TOOL_CACHE}/node/${NODE_VERSION}/${NODE_ARCH}"
+  rm -v node.tar.gz
   touch "${RUNNER_TOOL_CACHE}/node/${NODE_VERSION}/${NODE_ARCH}.complete"
 
   chown -R runner:docker "${RUNNER_TOOL_CACHE}/node"
