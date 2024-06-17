4aa3895ac0bd1dd0e13855d74627d5921f4e2eb8
Hidetake Iwata <int128@gmail.com>
2024-06-17 13:41:37 +0900

    Test modify-apt-sources scripts

--- entrypoint.d/modify-apt-sources.sh
+++ entrypoint.d/modify-apt-sources.sh
@@ -13,51 +13,16 @@ get_aws_availability_zone () {
   curl -sf -H "X-aws-ec2-metadata-token: $aws_imds_token" http://169.254.169.254/latest/meta-data/placement/availability-zone
 }
 
-modify_for_aws_x86_64_jammy () {
-  local aws_region="$(get_aws_region)"
-  echo "Modifying /etc/apt/sources.list for ${aws_region}" >&2
-  cat > /etc/apt/sources.list <<EOF
-deb http://${aws_region}.ec2.archive.ubuntu.com/ubuntu/ jammy main restricted
-deb http://${aws_region}.ec2.archive.ubuntu.com/ubuntu/ jammy-updates main restricted
-deb http://${aws_region}.ec2.archive.ubuntu.com/ubuntu/ jammy universe
-deb http://${aws_region}.ec2.archive.ubuntu.com/ubuntu/ jammy-updates universe
-deb http://${aws_region}.ec2.archive.ubuntu.com/ubuntu/ jammy multiverse
-deb http://${aws_region}.ec2.archive.ubuntu.com/ubuntu/ jammy-updates multiverse
-deb http://${aws_region}.ec2.archive.ubuntu.com/ubuntu/ jammy-backports main restricted universe multiverse
-deb http://security.ubuntu.com/ubuntu jammy-security main restricted
-deb http://security.ubuntu.com/ubuntu jammy-security universe
-deb http://security.ubuntu.com/ubuntu jammy-security multiverse
-EOF
-}
+local ubuntu_release="$(lsb_release -cs || true)"
+local platform="$(uname -m)"
+echo "Running on Ubuntu ${ubuntu_release} ${platform}" >&2
 
-modify_for_aws_aarch64_jammy () {
-  local aws_availability_zone="$(get_aws_availability_zone)"
-  echo "Modifying /etc/apt/sources.list for ${aws_availability_zone}" >&2
-  cat > /etc/apt/sources.list <<EOF
-deb http://${aws_availability_zone}.clouds.ports.ubuntu.com/ubuntu-ports/ jammy main restricted
-deb http://${aws_availability_zone}.clouds.ports.ubuntu.com/ubuntu-ports/ jammy-updates main restricted
-deb http://${aws_availability_zone}.clouds.ports.ubuntu.com/ubuntu-ports/ jammy universe
-deb http://${aws_availability_zone}.clouds.ports.ubuntu.com/ubuntu-ports/ jammy-updates universe
-deb http://${aws_availability_zone}.clouds.ports.ubuntu.com/ubuntu-ports/ jammy multiverse
-deb http://${aws_availability_zone}.clouds.ports.ubuntu.com/ubuntu-ports/ jammy-updates multiverse
-deb http://${aws_availability_zone}.clouds.ports.ubuntu.com/ubuntu-ports/ jammy-backports main restricted universe multiverse
-deb http://ports.ubuntu.com/ubuntu-ports jammy-security main restricted
-deb http://ports.ubuntu.com/ubuntu-ports jammy-security universe
-deb http://ports.ubuntu.com/ubuntu-ports jammy-security multiverse
-EOF
-}
+if [[ -z "${aws_imds_token}" ]]; then
+  exit
+fi
 
-modify_for_aws () {
-  local platform="$(uname -m)"
-  local ubuntu_release="$(lsb_release -cs || true)"
-  echo "Running on EC2 ${platform} ${ubuntu_release}" >&2
-  if [[ $platform == x86_64 && $ubuntu_release == jammy ]]; then
-    modify_for_aws_x86_64_jammy
-  elif [[ $platform == aarch64 && $ubuntu_release == jammy ]]; then
-    modify_for_aws_aarch64_jammy
-  fi
-}
-
-if [ -n "${aws_imds_token}" ]; then
-  modify_for_aws
+if [[ $ubuntu_release == jammy && $platform == x86_64 ]]; then
+  bash /entrypoint.d/modify-apt-sources-jammy-x86_64.sh "$(get_aws_region)"
+elif [[ $ubuntu_release == jammy && $platform == aarch64 ]]; then
+  bash /entrypoint.d/modify-apt-sources-jammy-aarch64.sh "$(get_aws_availability_zone)"
 fi
