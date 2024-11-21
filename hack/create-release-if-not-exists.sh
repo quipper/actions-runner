653cfb0a18e5b60925bcde0b625066301f8b7806
Hidetake Iwata <int128@gmail.com>
2024-11-21 10:20:19 +0900

    Refactor workflows (#596)

--- hack/create-release-if-not-exists.sh
+++ hack/create-release-if-not-exists.sh
@@ -1,27 +1,26 @@
 #!/bin/bash
 set -eux
 
-# find the runner version in Dockerfile
+set_comment_output () {
+  echo "comment=$*" >> "$GITHUB_OUTPUT"
+}
+
+# Find the runner version in Dockerfile
 actions_runner_version="$(grep RUNNER_VERSION= Dockerfile | cut -f2 -d=)"
 
-# post a comment if pull request
-if [[ $GITHUB_EVENT_NAME == pull_request ]]; then
-  if gh release view "$actions_runner_version"; then
-    gh pr comment "$GITHUB_HEAD_REF" --body-file - <<EOF
-:new: If you need to build a new runner image, [create a new release](https://github.com/quipper/actions-runner/releases/new) after merge.
-EOF
-    exit
+if gh release view "$actions_runner_version"; then
+  if [[ $GITHUB_EVENT_NAME == pull_request ]]; then
+    set_comment_output ":bulb: If you need a new version, [create a new release](https://github.com/quipper/actions-runner/releases/new) after merge."
   fi
-
-  gh pr comment "$GITHUB_HEAD_REF" --body-file - <<EOF
-:robot: GitHub Actions will automatically create a release ${actions_runner_version} after merge.
-EOF
   exit
 fi
 
-if gh release view "$actions_runner_version"; then
-  exit # already exists
+if [[ $GITHUB_EVENT_NAME == pull_request ]]; then
+  set_comment_output ":robot: GitHub Actions will automatically create a release ${actions_runner_version} after merge."
+  exit
 fi
 
-# create a release if not exists
+# Create a release if not exists
 GITHUB_TOKEN="$RELEASE_TOKEN" gh release create "$actions_runner_version" --generate-notes
+
+set_comment_output ":robot: GitHub Actions automatically created a new release ${actions_runner_version}"
